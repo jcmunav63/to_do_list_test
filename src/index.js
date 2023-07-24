5b4e10f47a54c9c77890bea24b20aa4ffa531523
jcmunav63 <jcmunav63@gmail.com>
2023-07-24 15:17:52 -0600

    Edit css & js files to style to-do list.

--- src/index.js
+++ src/index.js
@@ -6,27 +6,35 @@ import './style.css';
 //   { index: 3, name: 'Fix the laptop', completed: false },
 //   { index: 4, name: 'Hire a carpenter', completed: false }];
 
-const tasksLocal = [];
+let tasksLocal = [];
 
-window.loadTasksFromLocalStorage = () => {
+window.loadTasksToLocalStorage = () => {
   const text = JSON.stringify(tasksLocal);
   localStorage.setItem('tasks', text);
 };
 
-const createTaskElement = (task) => {
+window.loadTasksFromLocalStorage = () => {
+  tasksLocal = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
+};
+
+const displayTaskElement = (task) => {
   const taskItem = document.createElement('li');
   taskItem.classList.add('task-row');
+
+  const tIndex = document.createElement('span');
+  tIndex.classList.add('task-index');
+  tIndex.value = task.index;
 
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
 
-  const taskText = document.createElement('span');
+  const taskText = document.createElement('input');
   taskText.classList.add('task-text');
-  taskText.textContent = task.name;
+  taskText.value = task.name;
   if (task.completed) {
     taskText.classList.add('completed-task');
-  }
+  };
 
   const moreIcon = document.createElement('span');
   moreIcon.classList.add('more-icon');
@@ -35,6 +43,7 @@ const createTaskElement = (task) => {
   deleteIcon.classList.add('delete-icon');
   deleteIcon.innerHTML = '&#128465;';
 
+  taskItem.appendChild(tIndex);
   taskItem.appendChild(checkbox);
   taskItem.appendChild(taskText);
   taskItem.appendChild(moreIcon);
@@ -43,15 +52,39 @@ const createTaskElement = (task) => {
   return taskItem;
 };
 
+function createTaskElement(name1) {
+  const index = tasksLocal.length + 1;
+  const complete = false;
+  const taskString = `{ index: ${index}, name: ${name1}, completed: ${complete}}`;
+  tasksLocal.push(taskString);
+  localStorage.setItem('tasks', JSON.stringify(tasksLocal));
+}
+
+document.getElementById('add-task-btn').addEventListener('click', () => {
+  const taskInput = document.getElementById('task-input');
+  const taskName = taskInput.value.trim();
+  if (taskName !== '') {
+    // const newTask = TaskManager.addTask(taskName);
+    // const newTaskElement = createTaskElement(newTask);
+    // taskList.appendChild(newTaskElement);
+    createTaskElement(taskName);
+    document.location.reload();
+    taskInput.value = '';
+  };
+});
+
 const displayTasks = () => {
   const taskList = document.getElementById('task-list');
   if (tasksLocal.length > 0) {
     tasksLocal.forEach((task) => {
-      const taskElement = createTaskElement(task);
+      const taskElement = displayTaskElement(task);
       taskList.appendChild(taskElement);
     });
   }
 };
 
-loadTasksFromLocalStorage();
-displayTasks();
+window.onload = () => {
+  loadTasksFromLocalStorage();
+  displayTasks();
+  // loadTasksToLocalStorage();
+};
