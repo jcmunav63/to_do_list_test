891af2f0d1c1fc5598e26b326efc428ccbd0de03
jcmunav63 <jcmunav63@gmail.com>
2023-07-25 15:00:17 -0600

    Edit js fiel to correct indexes in array after deleting tasks.

--- src/adddelupd.js
+++ src/adddelupd.js
@@ -6,12 +6,18 @@ function createTaskElement(taskName, tasksLocal) {
   localStorage.setItem('tasks', JSON.stringify(tasksLocal));
 }
 
+function arrangeIndexes(tasksLocal) {
+  for (let i = 0; i < tasksLocal.length; i += 1) {
+    tasksLocal[i].index = (i + 1);
+  }
+}
+
 function deleteTaskElement(tasksLocal, parent, delBtn) {
   // const taskName = parent.getElementsByClassName('task-input')[0].value;
   const taskIndex = parent.getElementsByClassName('task-index')[0].value;
   tasksLocal = tasksLocal.filter((t) => t.index !== taskIndex);
+  arrangeIndexes(tasksLocal);
   localStorage.setItem('tasks', JSON.stringify(tasksLocal));
-
   delBtn.classList.add('hide');
   document.location.reload();
 }
